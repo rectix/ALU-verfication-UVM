8ba1aac885006f33e6dcb33f55520fe0de048eef
rectix <abdounhocine932@gmail.com>
2026-08-16 22:11:36 +0200

    add some comments

--- interface.sv
+++ interface.sv
@@ -4,9 +4,10 @@ interface alu_interface(input logic clock);
     logic [7:0] A;
     logic [7:0] B;
     logic [3:0] opcode;
-
+    //output pins
     logic [7:0] result;
     bit Carry;
+    
     
 
 endinterface: alu_interface
